48bee30d36390e8f2dcab163bf364a79e93969b4
AppleSun1121 <bubblemiyu@gmail.com>
2017-03-05 17:35:10 +0800

    新增Pokemon類別

--- CS280-Lecture1-Examples/Program.cs
+++ CS280-Lecture1-Examples/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace CS280_Lecture1_Examples
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("身高:{0}公尺 , 體重:{1}磅", pokemon.Height, pokemon.Weight);
         }
     }
 }
